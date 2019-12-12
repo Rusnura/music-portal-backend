7b9c2c9a2d2f21baf0fa92b6651f3bb85fe50b2c
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-12-12 23:39:55 +0700

    Added common controller and need to implements downloading song from URL

---
+++ src/main/java/server/controllers/CommonController.java
@@ -0,0 +1,18 @@
+package server.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger;
+
+@RestController
+public class CommonController {
+    private static final Logger LOGGER = Logger.getLogger(CommonController.class.getName());
+
+    @PostMapping(value = "/api/download")
+    public ResponseEntity<?> downloadSong(@RequestParam String url) {
+        // TODO: Release of downloading a song from URL
+        return null;
+    }
+}
