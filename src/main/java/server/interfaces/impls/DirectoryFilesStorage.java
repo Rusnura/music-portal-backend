918135eb63217cc9c3700c8d6157a3077ba17b7b
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-12-29 13:45:30 +0700

    Small improvements

--- src/main/java/server/interfaces/impls/DirectoryFilesStorage.java
+++ src/main/java/server/interfaces/impls/DirectoryFilesStorage.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,13 +29,19 @@ public class DirectoryFilesStorage implements IFilesStorage<File> {
         if (audioFilesDirectory.exists() && audioFilesDirectory.canWrite()) {
             File playlistDirectory = new File(audioFilesDirectory, subDirectoryName);
             if (!playlistDirectory.exists()) {
-                playlistDirectory.mkdir();
+                if (playlistDirectory.mkdir()) {
+					LOGGER.log(Level.WARNING, "Can't create a new playlist directory ! Playlist directory = " + playlistDirectory.getPath() +
+						", exists=" + playlistDirectory.exists() +
+						", canRead=" + playlistDirectory.canRead() +
+						", canWrite=" + playlistDirectory.canWrite());
+					throw new IncorrectAudioException("User data directory isn't writable");
+				}
             }
 
             if (playlistDirectory.canWrite()) {
                 try {
                     byte[] bytes = uploadedAudioFile.getBytes();
-                    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(audioFile = new File(playlistDirectory, uploadedAudioFile.getOriginalFilename())));
+                    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(audioFile = new File(playlistDirectory, Objects.requireNonNull(uploadedAudioFile.getOriginalFilename()))));
                     bos.write(bytes);
                     bos.close();
                 } catch (Exception e) {
