09913042475fdef192f2222ffed62afb64869b58
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-01-11 23:46:45 +0700

    RemoteURLFilesStorage partially released :D

--- src/main/java/server/interfaces/impls/RemoteURLFilesStorage.java
+++ src/main/java/server/interfaces/impls/RemoteURLFilesStorage.java
@@ -1,8 +1,11 @@
 package server.interfaces.impls;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import server.interfaces.IFilesStorage;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Logger;
@@ -17,7 +20,24 @@ public class RemoteURLFilesStorage implements IFilesStorage {
 
     @Override
     public String write(MultipartFile uploadedAudioFile, String subDirectoryName) throws IOException {
-        return null;
+    	String requestURL = remoteFileStorageUrl.toString() + "file?path=";
+		LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		// Hack for uploading multipart file to another resource
+		ByteArrayResource contentsAsResource = new ByteArrayResource(uploadedAudioFile.getBytes()) {
+			@Override
+			public String getFilename() {
+				return uploadedAudioFile.getOriginalFilename();
+			}
+		};
+		params.add("file", contentsAsResource);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(params, headers);
+		ResponseEntity<String> response = new RestTemplate().exchange(requestURL, HttpMethod.POST, requestEntity, String.class);
+		if (response.getStatusCode().is2xxSuccessful()) {
+			// TODO: Release code...
+		}
+		return null;
     }
 
     @Override
