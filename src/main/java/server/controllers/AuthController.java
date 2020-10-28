3baf84361c7acb521dd7b6c4c258ad42e1d4e777
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-10-29 00:00:03 +0700

    Removed excess lines

--- src/main/java/server/controllers/AuthController.java
+++ src/main/java/server/controllers/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.helpers.JwtHelper;
 import server.models.AuthRequest;
